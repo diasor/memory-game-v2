6371578d39d41a7e1356ec8b4a8bb05fe9eb9667
Diana Sormani <diana.sorman+git@gmail.com>
2020-12-25 11:02:46 +0100

    avatar component

---
+++ src/components/About/Avatar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="text-center">
+      <!-- <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template> -->
+
+      <v-card>
+          <v-img
+            max-height="150"
+            max-width="250"
+            src="../../assets/diana-avatar.png"
+            ></v-img>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                    color="primary"
+                    text
+                    @click="dialog = false"
+                >
+                    I accept
+                </v-btn>
+            </v-card-actions>
+      </v-card>
+  </div>
+</template>
+<script>
+export default {
+  name: "Avatar"
+}
+</script>
